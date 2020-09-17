c5b2f01bc119e01fb7c155ade66dafa75453be3f
jahn carlos ignacio velasquez <jahn24@hotmail.com>
2020-09-17 09:03:35 -0500

    Hola

---
+++ Usuarios/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Areas.Users.Controllers
+{
+    [Area("Users")] // especificamos al controlador que esta en la area Users
+    public class UsersController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        public ViewResult Users()
+        {
+            return View("Users");
+        }
+
+        public ViewResult Registrar()
+        {
+            return View("Registrar");
+        }
+    }
+}
